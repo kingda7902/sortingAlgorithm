128d067af34ade06f45c36c593d782ae9768ab7f
Jason Peng <mijasoda@gmail.com>
2021-02-01 16:54:22 +0800

    Implement the quicksort using implace version.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from sorting import quick_sort
+from sorting import quick_sort, quicksort
 from utils import timer, read_the_numbers
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # print_hi('PyCharm')
+
     file = 'numbers.txt'
     raw_data = read_the_numbers(file)
     data = list(map(int, raw_data))
 
     # data = timer(sorted)(data)
-    timer(quick_sort)(data, 0, len(data) - 1)
+    # timer(quick_sort)(data, 0, len(data) - 1)
+    timer(quicksort)(data, 0, len(data) - 1)
     print(data[:10])
--- sorting.py
+++ sorting.py
@@ -1,3 +1,29 @@
+def swap(a, left, right):
+    a[left], a[right] = a[right], a[left]
+
+
+def partition(a, left, right, pivot_index) -> int:
+    pivot_value = a[pivot_index]
+    swap(a, pivot_index, right)
+    store_index = left
+    for i in range(left, right):
+        if a[i] <= pivot_value:
+            swap(a, store_index, i)
+            store_index += 1
+
+    swap(a, right, store_index)
+    return store_index
+
+
+def quicksort(a, left, right):
+    """
+    The inplace version from the wikipedia
+    """
+    if right > left:
+        pivot_index = left
+        pivot_new_index = partition(a, left, right, pivot_index)
+        quicksort(a, left, pivot_new_index-1)
+        quicksort(a, pivot_new_index+1, right)
 
 
 def quick_sort(numbers_data, left, right):
@@ -15,7 +41,7 @@ def quick_sort(numbers_data, left, right):
             i += 1
         if i < j:
             numbers_data[i], numbers_data[j] = numbers_data[i], numbers_data[j]
-
-    quick_sort(numbers_data, left, i - 1)
-    quick_sort(numbers_data, i + 1, right)
+    #
+    # quick_sort(numbers_data, left, i - 1)
+    # quick_sort(numbers_data, i + 1, right)
 
